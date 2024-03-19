1d45a9c4bc56da9002b1d0b5151f5b6590fa086f
sourav250301 <157361833+sourav250301@users.noreply.github.com>
2024-03-19 17:19:20 +0530

    first work

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import MessageContainer from "../../components/messages/MessageContainer";
+import Sidebar from "../../components/sidebar/Sidebar";
+
+const Home = () => {
+	return (
+		<div className='flex sm:h-full md:h-full w-full rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
+		</div>
+	);
+};
+export default Home;
